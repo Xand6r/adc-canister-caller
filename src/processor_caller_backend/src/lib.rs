e36959f135bc1b3b4991b56f8cd855402062d9b4
xand6r <shuaibualexander@gmail.com>
2024-08-19 03:09:48 +0100

    boilerplate code

---
+++ src/processor_caller_backend/src/lib.rs
@@ -0,0 +1,28 @@
+use std::str::FromStr;
+
+use candid::Principal;
+use ic_cdk::{api::time, println};
+use types::PriceResponse;
+
+pub mod types;
+
+#[ic_cdk::update]
+async fn submit_price_request(currency_pairs: String) -> String {
+    let processor_canister_principal = Principal::from_str("bkyz2-fmaaa-aaaaa-qaaaq-cai").unwrap();
+
+    let (request_id,): (String,) = ic_cdk::call(
+        processor_canister_principal,
+        "request_prices",
+        (currency_pairs,),
+    )
+    .await
+    .unwrap();
+
+    // println!("{:?}", request_id)
+    return request_id;
+}
+
+#[ic_cdk::update]
+fn receive_price_response(response: PriceResponse) {
+    println!("receive_price_response: {:?}", response);
+}
